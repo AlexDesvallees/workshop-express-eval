ee1b592eafe5ad40e7c1270854d4e8b75b01aeda
AlexDesvallees <alex.desvallees@gmail.com>
2018-01-26 15:34:39 +0100

    Exercise 2

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ let chalk = require('chalk');
 const app = require('./src/app');
 
 // port
-const port = 3000;
+const port = app.listen(process.argv[2]);
+console.log(process.argv[2].toString())
 
 // start server here
 console.log(chalk.green('Hello web server'));
